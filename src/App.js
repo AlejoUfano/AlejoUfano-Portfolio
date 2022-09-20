9c3f1625381545c16710b3155a2deccd79757949
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-20 15:24:40 -0300

    Feat(All) Modularization and new dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/pages/Home/Home.jsx'
+import Skills from './Components/pages/Skills/Skills.jsx'
+import Projects from './Components/pages/Projects/Projects.jsx'
+import Contact from './Components/pages/Contact/Contact.jsx'
+import NavBar from './Components/organisms/NavBar/NavBar.jsx';
 
 function App() {
   return (
-    <div>
-      Portfolio
-    </div>
+    <>
+
+      <NavBar/>
+
+      <Routes>
+        <Route path='/' element={<Home />} />        
+        <Route path='/skills' element={<Skills />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+
+    </>
   );
 }
 
---
+++ src/Components/organisms/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProjectCard() {
+  return (
+    <div>ProjectCard</div>
+  )
+}
