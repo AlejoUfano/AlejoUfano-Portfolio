941b95e555e3b192e8a8991505dd94c55f0c7fcf
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-23 16:52:24 -0300

    Feat(Assets) Projects assets added

---
+++ src/utils/data/projects.js
@@ -0,0 +1,20 @@
+import dogs from '../../Assets/projects/dogsSpaAlejoUfano.png'
+import ecommerce from '../../Assets/projects/kembaEcommerceAlejoUfano.png'
+
+export const projects = [
+    {
+        img: dogs,
+        title: 'Dogs SPA',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        link: 'https://dogs-app-alpha.vercel.app/',
+        repository: 'https://github.com/AlejoUfano/PIHENRY/tree/main/PI-Dogs-main/PI-Dogs-main'
+
+    },
+    {
+        img: ecommerce,
+        title: 'E-commerce',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        link: 'https://pf-henry.vercel.app/',
+        repository: 'https://github.com/franRappazzini/PF---Henry'
+    }
+]
