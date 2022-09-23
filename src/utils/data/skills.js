691256b470cbc4034715808fd87bab36dcdf8422
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-22 21:56:26 -0300

    Feat(Assets) Assets related to skills added

---
+++ src/utils/data/skills.js
@@ -0,0 +1,117 @@
+import bootstrap from "../../Assets/skills/bootstrap.png";
+import css from "../../Assets/skills/css.png";
+import express from "../../Assets/skills/express.png";
+import git from "../../Assets/skills/git.png";
+import html from "../../Assets/skills/html.png";
+import javascript from "../../Assets/skills/js.png";
+import jest from "../../Assets/skills/jest.png";
+import jquery from "../../Assets/skills/jquery.png";
+import less from "../../Assets/skills/less.png";
+import mui from "../../Assets/skills/mui.png";
+import nodejs from "../../Assets/skills/nodejs.png";
+import npm from "../../Assets/skills/npm.png";
+import postgresql from "../../Assets/skills/postgresql.png";
+import react from "../../Assets/skills/reactjs.png";
+import redux from '../../Assets/skills/redux.png';
+import sass from "../../Assets/skills/sass.png";
+import sequelize from "../../Assets/skills/sequelize.png";
+import sql from "../../Assets/skills/sql.png";
+// import typescript from "../../assets/img/stack/typescript.png";
+
+export const skills = [
+  {
+    title: "JavaScript",
+    img: javascript,
+    href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+//   {
+//     title: "TypeScript",
+//     img: typescript,
+//     href: "https://www.typescriptlang.org/",
+//   },
+  {
+    title: "HTML",
+    img: html,
+    href: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    title: "CSS",
+    img: css,
+    href: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    title: "SQL",
+    img: sql,
+    href: "https://www.w3schools.com/sql/sql_intro.asp",
+  },
+  {
+    title: "React JS",
+    img: react,
+    href: "https://reactjs.org/",
+  },
+  {
+    title: "Redux",
+    img: redux,
+    href: "https://redux.js.org/",
+  },
+  {
+    title: "Node JS",
+    img: nodejs,
+    href: "https://nodejs.org/en/",
+  },
+  {
+    title: "Express",
+    img: express,
+    href: "https://expressjs.com/",
+  },
+  {
+    title: "jQuery",
+    img: jquery,
+    href: "https://jquery.com/",
+  },
+  {
+    title: "Sass",
+    img: sass,
+    href: "https://sass-lang.com/",
+  },
+  {
+    title: "Less",
+    img: less,
+    href: "https://lesscss.org/",
+  },
+  {
+    title: "PostgreSQL",
+    img: postgresql,
+    href: "https://www.postgresql.org/",
+  },
+  {
+    title: "Sequelize",
+    img: sequelize,
+    href: "https://sequelize.org/",
+  },
+  {
+    title: "Jest",
+    img: jest,
+    href: "https://jestjs.io/",
+  },
+  {
+    title: "Material UI",
+    img: mui,
+    href: "https://mui.com/",
+  },
+  {
+    title: "Bootstrap",
+    img: bootstrap,
+    href: "https://getbootstrap.com/",
+  },
+  {
+    title: "npm",
+    img: npm,
+    href: "https://www.npmjs.com/",
+  },
+  {
+    title: "Git",
+    img: git,
+    href: "https://git-scm.com/",
+  },
+];
