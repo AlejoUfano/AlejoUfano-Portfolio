7f52128251f50d9ed496c6985100ecfc04961ea0
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-23 16:53:44 -0300

    Feat(projectCard) projectCard Component added

--- src/Components/organisms/ProjectCard/ProjectCard.jsx
+++ src/Components/organisms/ProjectCard/ProjectCard.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import './projectCard.css'
+import Tilt from 'react-parallax-tilt'
 
-export default function ProjectCard() {
+const ProjectCard = ({img, title, description, link, repository}) => {
   return (
-    <div>ProjectCard</div>
+    <Tilt 
+    tiltMaxAngleX={10}
+    tiltMaxAngleY={10}
+    tiltReverse={false}
+    className='project__card-container'
+    >
+      <img src={img} title={title} alt={title} className="project-img" />
+      <div className="project__title">{title}</div>
+      <div className="project__description">{description}</div>
+      <div className="project__icons-container">
+
+        <a href={repository} className='project__icon' rel="noopener noreferrer" target='_blank'>
+          <i class="uil uil-github"></i>
+        </a>
+
+        <a href={link} className='project__icon' rel="noopener noreferrer" target='_blank'>
+          <i class="uil uil-external-link-alt"></i>
+        </a>
+
+      </div>
+
+    </Tilt>
   )
 }
+
+export default ProjectCard
