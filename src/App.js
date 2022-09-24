9383af94c33260dbc31ca8a20b39a80e64de6fe0
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-24 16:33:49 -0300

    Fix(App/index) Updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import Home from './Components/pages/Home/Home.jsx'
-import About from './Components/pages/About/About.jsx'
 import Skills from './Components/pages/Skills/Skills.jsx'
 import Projects from './Components/pages/Projects/Projects.jsx'
 import Contact from './Components/pages/Contact/Contact.jsx'
 import NavBar from './Components/organisms/NavBar/NavBar.jsx';
+import ScrollUp from './Components/organisms/ScrollUp/ScrollUp.jsx';
+import Footer from './Components/organisms/Footer/Footer.jsx';
 
 function App() {
   return (
     <>
-
       <NavBar />
       <main className='main'>
-       <Home />
-       <Skills />
-       <Projects />
+        <Home />
+        <Skills />
+        <Projects />
+        <Contact />
       </main>
-
+      <Footer />
+      <ScrollUp />
     </>
   );
 }
