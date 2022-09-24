ddacf9cac26c6504763f0cbddc2215a2522035b5
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-23 23:17:41 -0300

    Feat(Contact) Contact section component added

--- src/Components/pages/Contact/Contact.jsx
+++ src/Components/pages/Contact/Contact.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import './contact.css'
+import ContactCard from '../../organisms/ContactCard/ContactCard'
+import { contact } from '../../../utils/data/contact'
+import ContactForm from '../../organisms/ContactForm/ContactForm'
 
-export default function Contact() {
+const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact__global-container'>
+      <div className="contact__title">Get in touch</div>
+      <div className="contact__subtitle">Contact Me</div>
+
+      <div className="elements__container">
+
+        <div className="cards__container">
+          <div className="cards__title">Talk to me</div>
+          {
+            contact.map(c=>(
+              <ContactCard key={c.name} {...c}/>
+            ))
+          }
+        </div>
+        <div className="message__container">
+          <div className="message__title">Send me a message</div>
+            <ContactForm />
+        </div>
+      </div>
+      
+    </div>
   )
 }
+
+export default Contact
