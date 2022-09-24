2af9e18223ae8bc6e78556f0d0060e0edc9a6a9c
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-24 16:38:04 -0300

    Fix(Projects) Updates related to Projects section

--- src/Components/organisms/ProjectCard/ProjectCard.jsx
+++ src/Components/organisms/ProjectCard/ProjectCard.jsx
@@ -10,17 +10,17 @@ const ProjectCard = ({img, title, description, link, repository}) => {
     tiltReverse={false}
     className='project__card-container'
     >
-      <img src={img} title={title} alt={title} className="project-img" />
-      <div className="project__title">{title}</div>
-      <div className="project__description">{description}</div>
-      <div className="project__icons-container">
+      <img src={img} title={title} alt={title} className='project-img' />
+      <div className='project__title'>{title}</div>
+      <div className='project__description'>{description}</div>
+      <div className='project__icons-container'>
 
-        <a href={repository} className='project__icon' rel="noopener noreferrer" target='_blank'>
-          <i class="uil uil-github"></i>
+        <a href={repository} className='project__icon' rel='noopener noreferrer' target='_blank'>
+          <i class='uil uil-github'></i>
         </a>
 
-        <a href={link} className='project__icon' rel="noopener noreferrer" target='_blank'>
-          <i class="uil uil-external-link-alt"></i>
+        <a href={link} className='project__icon' rel='noopener noreferrer' target='_blank'>
+          <i class='uil uil-external-link-alt'></i>
         </a>
 
       </div>
