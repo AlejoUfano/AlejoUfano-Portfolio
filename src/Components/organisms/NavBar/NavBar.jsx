d7070fcee95d05265384507e23c0423bcc1ad7b3
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-24 16:39:54 -0300

    Fix(NavBar) NavBar fixes

--- src/Components/organisms/NavBar/NavBar.jsx
+++ src/Components/organisms/NavBar/NavBar.jsx
@@ -1,43 +1,44 @@
 import React, { useState } from 'react'
-import './navBar.css'                                                                                                                                                                                                
+import './navBar.css'
+import { Link } from 'react-scroll'                                                                                                                                                                                           
 
 export default function NavBar() {
     let [Toggle, showMenu] = useState(false)
   return (
-    <header className="header">
-        <nav className="nav container">
-            <a href="index.html" className="nav__logo">{'<Au.dev/>'}</a>
+    <header className='header'>
+        <nav className='nav container'>
+        <Link to='home' spy={true} smooth={true} offset={-50} duration={500} className='nav__logo'>{'<Au.dev/>'}</Link>
             <div className={Toggle ? 'nav__menu show-menu' : 'nav__menu'}>
-                <ul className="nav__list grid">
+                <ul className='nav__list grid'>
 
-                    <li className="nav__item">
-                        <a href="" className="nav__link active-link">
-                            <i className="uil uil-estate nav__icon"></i> Home
-                        </a>
+                    <li className='nav__item'>
+                        <Link to='home' spy={true} smooth={true} offset={-50} duration={500} className='nav__link'>
+                            <i className='uil uil-estate nav__icon'></i> Home
+                        </Link>
                     </li>
 
-                    <li className="nav__item">
-                        <a href="" className="nav__link">
-                            <i className="uil uil-file-alt nav__icon"></i> Skills
-                        </a>
+                    <li className='nav__item'>
+                        <Link to='skills' spy={true} smooth={true} offset={0} duration={500} className='nav__link'>
+                            <i className='uil uil-file-alt nav__icon'></i> Skills
+                        </Link>
                     </li>
 
-                    <li className="nav__item">
-                        <a href="" className="nav__link">
-                            <i className="uil uil-scenery nav__icon"></i> Projects
-                        </a>
+                    <li className='nav__item'>
+                        <Link to='projects' spy={true} smooth={true} offset={0} duration={500} className='nav__link'>
+                            <i className='uil uil-scenery nav__icon'></i> Projects
+                        </Link>
                     </li>
 
-                    <li className="nav__item">
-                        <a href="" className="nav__link">
-                            <i className="uil uil-message nav__icon"></i> Contact
-                        </a>
+                    <li className='nav__item'>
+                        <Link to='contact' spy={true} smooth={true} offset={0} duration={500} className='nav__link'>
+                            <i className='uil uil-message nav__icon'></i> Contact
+                        </Link>
                     </li>
                 </ul>
-                <i class="uil uil-times nav__close" onClick={()=>showMenu(!Toggle)}></i>
+                <i class='uil uil-times nav__close' onClick={()=>showMenu(!Toggle)}></i>
             </div>
             <div className='nav__toggle' onClick={()=>showMenu(!Toggle)}>
-                <i class="uil uil-apps"></i>
+                <i class='uil uil-apps'></i>
             </div>
         </nav>
     </header>
