ca55caf0975739a5c7e96cb28eeb452c4c2b8ab0
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-24 16:41:04 -0300

    Fix(Home) Updates related to Home section

--- src/Components/organisms/Data/Data.jsx
+++ src/Components/organisms/Data/Data.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from 'react-scroll' 
 
 const Data = () => {
   return (
-    <div className="data__container">
-        <h1 className="home__title title">Alejo Ufano
+    <div className='data__container'>
+        <h1 className='home__title title'>Alejo Ufano
         </h1>
-        <h3 className="home__subtitle subtitle">
-        <div className="subtitle__line"></div>Full Stack Developer
+        <h3 className='home__subtitle subtitle'>
+        <div className='subtitle__line'></div>Full Stack Developer
         </h3>
-        <p className="home__description description">
+        <p className='home__description description'>
             I am a very committed, passionate and constantly learning developer.
         </p>
-        <a href="#contact" className="button button--flex">
-            Contact Me  <i class="uil uil-message" style={{fontSize: '23px',marginLeft:'.2rem'}}></i>
-        </a>
+        <Link to='contact' spy={true} smooth={true} offset={0} duration={500} className='button button--flex'>
+            Contact Me  <i class='uil uil-message' style={{fontSize: '23px',marginLeft:'.2rem'}}></i>
+        </Link>
     </div>
   )
 }
