7b48f39329d214096656a851775c4f196e651b5e
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-26 13:12:45 -0300

    Fix(Models) Changes related to models

--- src/Components/pages/Home/Home.jsx
+++ src/Components/pages/Home/Home.jsx
@@ -3,20 +3,20 @@ import './home.css'
 import Social from '../../organisms/Social/Social'
 import Data from '../../organisms/Data/Data'
 import Model from '../../organisms/Model/Model'
-import Model2 from '../../organisms/Model/Model2'
+import Droned from '../../organisms/Model/Droned'
 
 const Home = ({ darkmode }) => {
   return (
     <div className='home__container' id='home'>
     <div className='home__wrapper'>
-      <div className= { darkmode ? 'social__container' : 'social__container2' }>
+      <div className='social__container'>
         <Social />
       </div>  
       <div className='info__wrapper'>
         <div className='info__container'>
           <Data />
         </div>
-        { darkmode ? <Model /> : <Model2 /> }
+        { darkmode ? <Model /> : <Droned /> }
       </div>      
     </div>      
     </div>
