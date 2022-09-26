0ef7a2533eb6dc6d7457b4d42358db03bc986fe2
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-26 00:01:52 -0300

    Fix(App) App final update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import React, { useState } from 'react'
 import Home from './Components/pages/Home/Home.jsx'
 import Skills from './Components/pages/Skills/Skills.jsx'
 import Projects from './Components/pages/Projects/Projects.jsx'
 import Contact from './Components/pages/Contact/Contact.jsx'
 import NavBar from './Components/organisms/NavBar/NavBar.jsx';
 import ScrollUp from './Components/organisms/ScrollUp/ScrollUp.jsx';
-import Footer from './Components/organisms/Footer/Footer.jsx';
 
 function App() {
+  let [darkmode, setDarkmode] = useState('light')
   return (
     <>
-      <NavBar />
+      <NavBar setDarkmode={setDarkmode} darkmode={darkmode}/>
       <main className='main'>
-        <Home />
+        <Home darkmode={darkmode} />
         <Skills />
         <Projects />
         <Contact />
       </main>
-      {/* <Footer /> */}
       <ScrollUp />
     </>
   );
