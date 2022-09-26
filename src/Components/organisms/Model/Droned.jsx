71a002b6fcd1920669d9914d8caa723ba6bbbe6b
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-26 13:10:59 -0300

    Feat(Model) Model changed for darkmode

---
+++ src/Components/organisms/Model/Droned.jsx
@@ -0,0 +1,22 @@
+import React, { Suspense, useRef } from 'react'
+import './droned.css'
+import { NewDrone } from './NewDrone'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stage } from '@react-three/drei'
+
+const Droned = () => {
+    const ref = useRef()
+  return (
+    <div className='droned'>
+        <Canvas shadows dpr={[1, 10]} camera={{ fov: 70 }}>
+        <Suspense fallback={null}>          
+          <Stage contactShadow shadows adjustCamera intensity={1} environment="city" preset="rembrandt" controls={ref}>
+            <NewDrone />
+          </Stage>
+        </Suspense>
+        <OrbitControls ref={ref} autoRotate />
+      </Canvas>
+    </div>
+  )
+}
+export default Droned
