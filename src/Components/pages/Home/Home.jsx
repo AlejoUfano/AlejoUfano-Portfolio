b9e9a0aa7bebdda21a74fa997dc4fbc602cc456b
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-26 00:09:54 -0300

    Fix(Home) Home final update

--- src/Components/pages/Home/Home.jsx
+++ src/Components/pages/Home/Home.jsx
@@ -1,23 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './home.css'
 import Social from '../../organisms/Social/Social'
 import Data from '../../organisms/Data/Data'
 import Model from '../../organisms/Model/Model'
 import Model2 from '../../organisms/Model/Model2'
 
-const Home = () => {
-  let [model, setModel] = useState('drone')
+const Home = ({ darkmode }) => {
   return (
     <div className='home__container' id='home'>
     <div className='home__wrapper'>
-      <div className= {model==='drone' ? 'social__container' : 'social__container2'}>
+      <div className= { darkmode ? 'social__container' : 'social__container2' }>
         <Social />
       </div>  
       <div className='info__wrapper'>
         <div className='info__container'>
           <Data />
         </div>
-        { model==='drone' ? <Model /> : <Model2 /> }
+        { darkmode ? <Model /> : <Model2 /> }
       </div>      
     </div>      
     </div>
