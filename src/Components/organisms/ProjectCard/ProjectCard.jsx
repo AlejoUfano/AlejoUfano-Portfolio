6d5f8b0ec54f9203ff45dba0fe9f298ed03f7ba2
Alejo Ufano <ufanoalejo@gmail.com>
2023-08-09 18:39:52 -0300

    Update(Screen/Comp) Projects & ProjectCard updated

--- src/Components/organisms/ProjectCard/ProjectCard.jsx
+++ src/Components/organisms/ProjectCard/ProjectCard.jsx
@@ -1,32 +1,79 @@
-import React from 'react'
-import './projectCard.css'
-import Tilt from 'react-parallax-tilt'
+import React from "react";
+import "./projectCard.css";
+import Tilt from "react-parallax-tilt";
+import appStoreIcon from "../../../Assets/appstore.png";
+import playStoreIcon from "../../../Assets/playstore.png";
+import { toast } from "react-hot-toast";
 
-const ProjectCard = ({img, title, description, link, repository}) => {
+const ProjectCard = ({
+  img,
+  title,
+  description,
+  link,
+  repository,
+  appstore,
+  playstore,
+  app,
+}) => {
+  const openLink = (url) => {
+    console.log("url length", url.length);
+    !url.length
+      ? toast("IOS Release coming soon!.", {
+          icon: "👏",
+          style: {
+            borderRadius: "5px",
+            background: "#333",
+            color: "#fff",
+          },
+        })
+      : window.open(url, "_blank", "noreferrer");
+  };
   return (
-    <Tilt 
-    tiltMaxAngleX={10}
-    tiltMaxAngleY={10}
-    tiltReverse={false}
-    className='project__card-container'
+    <Tilt
+      tiltMaxAngleX={10}
+      tiltMaxAngleY={10}
+      tiltReverse={false}
+      className="project__card-container"
     >
-      <img src={img} title={title} alt={title} className='project-img' />
-      <div className='project__title'>{title}</div>
-      <div className='project__description'>{description}</div>
-      <div className='project__icons-container'>
+      <img src={img} title={title} alt={title} className="project-img" />
+      <div className="project__title">{title}</div>
+      <div className="project__description">{description}</div>
+      <div className="project__icons-container">
+        {app ? (
+          <img
+            onClick={() => openLink(appstore)}
+            src={appStoreIcon}
+            title="appstore"
+            alt="appstore"
+            className="storeIcons"
+          />
+        ) : (
+          <a
+            href={repository}
+            className="project__icon"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i class="uil uil-github"></i>
+          </a>
+        )}
 
-        <a href={repository} className='project__icon' rel='noopener noreferrer' target='_blank'>
-          <i class='uil uil-github'></i>
-        </a>
-
-        <a href={link} className='project__icon' rel='noopener noreferrer' target='_blank'>
-          <i class='uil uil-external-link-alt'></i>
-        </a>
-
+        {app ? (
+          <img
+            onClick={() => openLink(playstore)}
+            src={playStoreIcon}
+            title="playstore"
+            alt="playstore"
+            className="storeIcons"
+          />
+        ) : (
+          <a href={link} className="project__icon">
+            <i class="uil uil-external-link-alt"></i>
+          </a>
+        )}
       </div>
-
     </Tilt>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/Components/pages/Projects/Projects.jsx
+++ src/Components/pages/Projects/Projects.jsx
@@ -1,53 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import './projects.css'
-import { projects } from '../../../utils/data/projects.js'
-import ProjectCard from '../../organisms/ProjectCard/ProjectCard.jsx'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import { Navigation, EffectFade } from 'swiper'
-import 'swiper/css/navigation'
-import 'swiper/css/effect-fade'
-import { motion } from 'framer-motion';
+import React, { useEffect, useState } from "react";
+import "./projects.css";
+import { projects } from "../../../utils/data/projects.js";
+import ProjectCard from "../../organisms/ProjectCard/ProjectCard.jsx";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation, EffectFade } from "swiper";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+import { motion } from "framer-motion";
 
 export default function Projects() {
-  const [width, setWidth] = useState(window.innerWidth)
+  const [width, setWidth] = useState(window.innerWidth);
   useEffect(() => {
     function handleResize() {
-      setWidth(window.innerWidth)    
-}
-    window.addEventListener('resize', handleResize)
-  },[])
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+  }, []);
   return (
-    <div className='projects__global' id='projects' >
-      <div className='projects__title title'>Projects</div>
+    <div className="projects__global" id="projects">
+      <div className="projects__title title">Projects</div>
       {/* <div className='projects__subtitle'>Recent Projects</div> */}
-      <motion.div 
-      className='projects__cards-container'
-      initial='hidden'
-      whileInView='visible'
-      viewport={{ once: true, amount: 0.5 }}
-      transition={{ duration: 0.4 }}
-      variants={{
-        hidden: { opacity: 0, x:-50 },
-        visible: { opacity: 1, x: 0},
-      }}
+      <motion.div
+        className="projects__cards-container"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.4 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
       >
-        
         <Swiper
-          modules = {[Navigation, EffectFade]}
+          modules={[Navigation, EffectFade]}
           navigation
           effect
-          speed = {800}
-          slidesPerView = { width > 768 ? 2 : 1 }
+          speed={800}
+          slidesPerView={width > 768 ? 2 : 1}
           loop
-          className='projects__slider'
+          className="projects__slider"
         >
-          {projects.map(p=>(
-            <SwiperSlide className='swiper__slide'><ProjectCard key={p.title} {...p}/></SwiperSlide>
+          {projects.map((p) => (
+            <SwiperSlide key={p.title} className="swiper__slide">
+              <ProjectCard key={p.title} {...p} />
+            </SwiperSlide>
           ))}
         </Swiper>
-
       </motion.div>
     </div>
-  )
+  );
 }
