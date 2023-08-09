7b60e3af785af77d17f43e3af5e422e7e3808c82
Alejo Ufano <ufanoalejo@gmail.com>
2023-08-09 18:37:01 -0300

    Feat(General) Toast dependency added & setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
-import Home from './Components/pages/Home/Home.jsx'
-import Skills from './Components/pages/Skills/Skills.jsx'
-import Projects from './Components/pages/Projects/Projects.jsx'
-import Contact from './Components/pages/Contact/Contact.jsx'
-import NavBar from './Components/organisms/NavBar/NavBar.jsx';
-import ScrollUp from './Components/organisms/ScrollUp/ScrollUp.jsx';
+import React, { useState } from "react";
+import Home from "./Components/pages/Home/Home.jsx";
+import Skills from "./Components/pages/Skills/Skills.jsx";
+import Projects from "./Components/pages/Projects/Projects.jsx";
+import Contact from "./Components/pages/Contact/Contact.jsx";
+import NavBar from "./Components/organisms/NavBar/NavBar.jsx";
+import ScrollUp from "./Components/organisms/ScrollUp/ScrollUp.jsx";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  let [darkmode, setDarkmode] = useState('light')
+  let [darkmode, setDarkmode] = useState("light");
   return (
     <>
-      <NavBar setDarkmode={setDarkmode} darkmode={darkmode}/>
-      <main className='main'>
+      <Toaster position="bottom-left" reverseOrder={false} />
+      <NavBar setDarkmode={setDarkmode} darkmode={darkmode} />
+      <main className="main">
         <Home darkmode={darkmode} />
         <Skills />
         <Projects />
